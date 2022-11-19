18e06b28cb8562f6e42e5ef43bf2a0f3daa2094a
AbhayGupta39 <abhaygupta1400@gmail.com>
2022-11-20 02:25:14 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h2 className="header--title">Business Card Template</h2>
+            <h4 className="header--project">Abhay Gupta's Card</h4>
+        </header>
+    )
+}
